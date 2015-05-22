1e3f2dcdb49c418331344ccab6b615c15c640f7d
Eeshit Dhaval Vaishnav <eeshit.vaishnav@gmail.com>
2015-05-21 17:02:20 -0700

    Comments. Important ones
    %Random sizes and times for 1D initialization.
    %Now what I am going to do is, I am going to have a ruleset and then
    when I
    %am going to compare the results for graphs that have cycles and graphs
    %that do not have cycles! And then I will change the initializations and
    %the rulesets to see where that takes us.

--- 6_states.m
+++ 6_states.m
@@ -1,10 +1,18 @@
 %I have 6 states from that email. Lets see what I can do with them. Hmm...
+
+%Random sizes and times for 1D initialization.
+%Now what I am going to do is, I am going to have a ruleset and then when I
+%am going to compare the results for graphs that have cycles and graphs
+%that do not have cycles! And then I will change the initializations and
+%the rulesets to see where that takes us.
+
 size = 101;
 time = 51;
 
 %
 A = zeros(time,size);
-A(1,floor(size/2) + 1)=1;
+
+%One way to initialize the cells -> A(1,floor(size/2) + 1)=1;
 
 %Ruleset for the 8bit two state automaton. Rule 90 is just the binary
 %representation of 90 01011010
